43177a9b32cd48ebf98cd9f9d961d137b4faedc1
RathiAaditya <f20202191@hyderabad.bit-pilani.ac.in>
2023-04-05 18:46:11 +0530

    Main and CUR

---
+++ cur.py
@@ -0,0 +1,94 @@
+import pandas as pd
+import numpy as np
+
+
+def CUR_decomposition(A):
+    """
+    A = CUR
+    A: m x n matrix
+    C: m x r matrix
+    U: r x r matrix
+    R: r x n matrix
+    """
+    # Get the shape of the matrix
+    m, n = A.shape
+    # Get the number of rows and columns
+    r = int(m/10)
+
+    # Get the row and column probabilities
+    row_prob = np.sum(A**2, axis=1)
+    row_prob = row_prob/np.sum(row_prob)
+    col_prob = np.sum(A**2, axis=0)
+    col_prob = col_prob/np.sum(col_prob)
+
+    # Get the row and column indices
+    row_indices = np.random.choice(m, r, replace=False, p=row_prob)
+    col_indices = np.random.choice(n, r, replace=False, p=col_prob)
+    # Get the row and column matrices
+    R = A[row_indices, :]
+    C = A[:, col_indices]
+    W = A[row_indices, :][:, col_indices]
+
+    print("This is R")
+    print(R)
+    print("This is C")
+    print(C)
+    X, Z, Y = np.linalg.svd(W)
+    # Take reciprocal of non zero values in Z
+    Z = 1/Z
+
+    # Make Z diagonal
+    Z = np.diag(Z)
+    # print(Z)
+    print(X.shape, Z.shape, Y.shape)
+    U = Y.T @ (Z**2) @ X.T
+    print(U.shape)
+    return C, U, R
+
+
+train_matrix = pd.read_csv("dataset/matrix.csv")
+train_matrix = train_matrix.drop('Unnamed: 0', axis=1)
+# print(train_matrix.head())
+
+# Convert to numpy array
+train_matrix = train_matrix.values
+A = np.array(train_matrix)
+C, U, R = CUR_decomposition(A)
+prediction_matrix = C @ U @ R
+# print(prediction_matrix)
+# Save the prediction matrix
+np.savetxt("dataset/prediction_matrix.csv", prediction_matrix, delimiter=",")
+print(prediction_matrix)
+
+# df = pd.io.parsers.read_csv('dataset/ratings.dat', names=[
+#                             'userid', 'movieid', 'ratings', 'time'], encoding='latin-1', engine='python', delimiter='::')
+# train_df = pd.read_csv("dataset/train_ratings.csv")
+# unique_users = df['userid'].unique()
+# unique_movies = df['movieid'].unique()
+# print(len(unique_users), len(unique_movies))
+# unique_users = train_df['userid'].unique()
+# unique_movies = train_df['movieid'].unique()
+# print(len(unique_users), len(unique_movies))
+
+
+def create_matrix():
+    df = pd.io.parsers.read_csv('dataset/ratings.dat', names=[
+                                'userid', 'movieid', 'ratings', 'time'], encoding='latin-1', engine='python', delimiter='::')
+    train_df = pd.read_csv("dataset/train_ratings.csv")
+    unique_users = df['userid'].unique()
+    unique_movies = df['movieid'].unique()
+
+    # Create a matrix
+    matrix = pd.DataFrame(index=unique_users, columns=unique_movies)
+    # Fill the matrix with the ratings
+    for i in range(len(train_df)):
+        matrix.loc[train_df['userid'][i],
+                   train_df['movieid'][i]] = train_df['ratings'][i]
+    # Fill the NaN values with 0
+    matrix.fillna(value=0, inplace=True)
+    # Save the matrix
+    matrix.to_csv("dataset/matrix.csv")
+    print(matrix)
+
+    # 6040, 3685
+# create_matrix()
--- main.py
+++ main.py
@@ -1,5 +1,87 @@
+import numpy as np
+import pandas as pd
+# from scipy.spatial.distance import cosine
+from sklearn.model_selection import train_test_split
+
+
+class Dataset:
+    def __init__(self) -> None:
+        self.ratings = pd.io.parsers.read_csv('dataset/ratings.dat', names=[
+                                              'userid', 'movieid', 'ratings', 'time'], encoding='latin-1', engine='python', delimiter='::')
+        self.ratings = self.ratings.drop('time', axis=1)
+        self.split_ratings()
+        count = self.test_ratings.groupby(['userid'])['userid'].count()
+        print(min(count))
+        # self.matrix = np.zeros((6040, 3952))
+        # for i, j in zip(self.train_ratings['userid'], self.train_ratings['movieid']):
+        #     self.matrix[i-1][j-1] = self.train_ratings['ratings']
+        # self.movie_data = pd.io.parsers.read_csv('dataset/movies.dat', names=['movie_id', 'title', 'genre'], encoding='latin-1', engine='python', delimiter='::')
+        # self.user_data = pd.io.parsers.read_csv('dataset/users.dat', names=['user_id', 'gender', 'age', 'occupation', 'zipcode'], encoding='latin-1', engine='python', delimiter='::')
+
+    def get_matrix(self):
+        matrix = pd.read_csv("dataset/matrix.csv")
+        self.matrix = np.array(matrix.values)
+
+    def split_ratings(self, test_size=0.2):
+        self.train_ratings, self.test_ratings = train_test_split(
+            self.ratings, test_size=test_size, stratify=self.ratings['userid'])
+        self.test_ratings.sort_values(by=['userid'], inplace=True)
+        self.train_ratings = self.train_ratings.reset_index(drop=True)
+        self.test_ratings = self.test_ratings.reset_index(drop=True)
+        self.train_ratings.to_csv('dataset/train_ratings.csv')
+        self.test_ratings.to_csv('dataset/test_ratings.csv')
 
 
 class ColabrativeFiltering:
-    def __init__(self, mat):
-        self.mat = mat
+    def __init__(self, matrix, k=10):
+        self.matrix = matrix
+        self.k = k
+
+    def get_correlation_coef(x, y):
+        return (1 - np.corrcoef(x, y)[0, 1])
+
+    def get_top_k_users(self, userid):
+        dist = []
+        for i in range(self.matrix.shape[0]):
+            dist.append(self.get_correlation_coef(
+                self.matrix[userid - 1], self.matrix[i]))
+        dist = np.array(dist)
+        sorted = np.argsort(dist)[:self.k]
+        return sorted, [dist[i] for i in sorted]
+
+    def get_rating(self, userid, movieid):
+        top_k_users, dist = self.get_top_k_users(userid)
+        rating = 0
+        for i in range(len(self.k)):
+            rating += self.matrix[top_k_users[i] -
+                                  1][movieid - 1] * (1 - dist[i])
+        return rating / (len(self.k)-sum(dist))
+
+    def get_RMSE(self, test_ratings):
+        rmse = 0
+        for i, j in zip(test_ratings['userid'], test_ratings['movieid']):
+            rmse += (self.get_rating(i, j) - test_ratings['ratings'])**2
+        return np.sqrt(rmse / len(test_ratings))
+
+    def get_precision_on_top_k(self, test_ratings, k):
+        precision = 0
+        count = test_ratings.group_by(['userid'])['userid'].count()[1]
+        print(count)
+        for i in range(1, len(count)+1):
+            for j in test_ratings['movieid']:
+
+        for i, j in zip(test_ratings['userid'], test_ratings['movieid']):
+            top_k_users, dist = self.get_top_k_users(i)
+            if j in top_k_users:
+                precision += 1
+        return precision / len(test_ratings)
+
+    def get_spearman_rank(self, test_ratings):
+        rank = []
+        for i, j in zip(test_ratings['userid'], test_ratings['movieid']):
+            top_k_users, dist = self.get_top_k_users(i)
+            rank.append(top_k_users.index(j))
+            return np.mean(rank)
+
+
+data = Dataset()
