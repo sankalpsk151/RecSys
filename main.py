Precision and Colabrative fixed

main.py
@@ -1,21 +1,32 @@
 import numpy as np
 import pandas as pd
-# from scipy.spatial.distance import cosine
+from scipy.spatial.distance import cosine
 from sklearn.model_selection import train_test_split
+from time import time
 
 
 class Dataset:
     def __init__(self) -> None:
+        t0 = time()
         self.ratings = pd.io.parsers.read_csv('dataset/ratings.dat', names=[
                                               'userid', 'movieid', 'ratings', 'time'], encoding='latin-1', engine='python', delimiter='::')
         self.ratings = self.ratings.drop('time', axis=1)
+        print(f"Time to read from dataset is {time() - t0} seconds")
+        
+        t0 = time()
         self.split_ratings()
         self.test_count = self.test_ratings.groupby(['userid'])['userid'].count()
-        self.test_count = self.test_ratings.groupby(['userid'])['userid'].count()
-        print(min(self.test_count), min(self.train_count))
-        # self.matrix = np.zeros((6040, 3952))
-        # for i, j in zip(self.train_ratings['userid'], self.train_ratings['movieid']):
-        #     self.matrix[i-1][j-1] = self.train_ratings['ratings']
+        self.train_count = self.train_ratings.groupby(['userid'])['userid'].count()
+        print(f"Number of users in test and train are {len(self.test_count), len(self.train_count)}")
+        print(f"Min movies for all users in test and train are {min(self.test_count), min(self.train_count)}")
+        print(f"Time to split and sort is {time() - t0} seconds")
+        # self.get_matrix()
+        
+        t0 = time()
+        self.matrix = np.zeros((6040, 3952))
+        for p, q, r in zip(self.train_ratings['userid'], self.train_ratings['movieid'], self.train_ratings['ratings']):
+            self.matrix[p-1][q-1] = r
+        print(f"Time to fill matrix {time() - t0} seconds")
         
         
         # self.movie_data = pd.io.parsers.read_csv('dataset/movies.dat', names=['movie_id', 'title', 'genre'], encoding='latin-1', engine='python', delimiter='::')
@@ -32,8 +43,8 @@ class Dataset:
         self.test_ratings.sort_values(by=['userid', 'ratings'], inplace=True)
         self.train_ratings = self.train_ratings.reset_index(drop=True)
         self.test_ratings = self.test_ratings.reset_index(drop=True)
-        self.train_ratings.to_csv('dataset/train_ratings.csv')
-        self.test_ratings.to_csv('dataset/test_ratings.csv')
+        # self.train_ratings.to_csv('dataset/train_ratings.csv')
+        # self.test_ratings.to_csv('dataset/test_ratings.csv')
 
 
 class ColabrativeFiltering:
@@ -42,43 +53,44 @@ class ColabrativeFiltering:
         self.k = k
         self.train_ratings = train
         self.test_ratings = test
-
-    def get_correlation_coef(x, y):
-        return (1 - np.corrcoef(x, y)[0, 1])
-
-    def get_top_k_users(self, userid):
-        dist = []
-        for i in range(self.matrix.shape[0]):
-            dist.append(self.get_correlation_coef(
-                self.matrix[userid - 1], self.matrix[i]))
-        dist = np.array(dist)
-        sorted = np.argsort(dist)[:self.k]
-        return sorted, [dist[i] for i in sorted]
-
+        self.get_top_k_users()
+        self.get_results()
+            
+    def get_top_k_users(self):
+        row_sums = np.linalg.norm(self.matrix, axis=1)
+        sim_mat = np.matmul(self.matrix, self.matrix.T) / np.matmul(row_sums[:, np.newaxis], row_sums[:, np.newaxis].T)
+        self.top_k_users = np.argsort(sim_mat, axis=1)[:, -(self.k + 1):-1]
+        self.top_k_sim =  np.take_along_axis(sim_mat, self.top_k_users, axis = 1)
+        
     def get_rating(self, userid, movieid):
-        top_k_users, dist = self.get_top_k_users(userid)
-        rating = 0
-        for i in range(len(self.k)):
-            rating += self.matrix[top_k_users[i] -
-                                  1][movieid - 1] * (1 - dist[i])
-        return rating / (len(self.k)-sum(dist))
-
+        rating = np.average(self.matrix[self.top_k_users[userid - 1], movieid - 1].reshape(self.k,), weights=self.top_k_sim[userid - 1])
+        return rating
+        
     def get_results(self):
+        t0 = time()
         self.pred_train = [ self.get_rating(i, j) for i, j in zip(self.train_ratings['userid'], self.train_ratings['movieid'])]
+        print(f"Time taken to predict using Collabrative Filtering Train: {time() - t0} seconds")
+        
+        t0 = time()
         self.pred_test = [ self.get_rating(i, j) for i, j in zip(self.test_ratings['userid'], self.test_ratings['movieid'])]
+        print(f"Time taken to predict using Collabrative Filtering Test: {time() - t0} seconds")
         
 class EvaluttionMetrics:
     def __init__(self) -> None:
         pass
     
     def get_RMSE(self, y, y_pred):
-        rmse = 0
-        for i, j in zip(y, y_pred):
-            rmse += (i - j)**2
-        return rmse
+        return np.sqrt(np.mean((y - y_pred)**2))
 
-    def get_precision_on_top_k(self, test_ratings, k):
-        
+    def get_precision_on_top_k(self, y, y_pred, count, top_k):
+        x = 0
+        map_k = 0
+        for movie_count in count:
+            _temp = np.argsort(y_pred[x:x+movie_count])[:-1]
+            for i in range(1, top_k + 1):
+                map_k += (_temp[:i] < i).sum() / i
+            x += movie_count
+        return map_k / (len(count) * top_k)       
 
     def get_spearman_rank(self, test_ratings):
         rank = []
@@ -89,3 +101,10 @@ class EvaluttionMetrics:
 
 
 data = Dataset()
+cf = ColabrativeFiltering(data.matrix, data.train_ratings, data.test_ratings, 15)
+ev = EvaluttionMetrics()
+print(f"Training RMSE for Collabrative filtering: {ev.get_RMSE(cf.train_ratings['ratings'], cf.pred_train)}")
+print(f"Test RMSE for Collabrative filtering: {ev.get_RMSE(cf.test_ratings['ratings'], cf.pred_test)}")
+top_k = 4
+print(f"Test MAP@{top_k} for Collabrative filtering: {ev.get_precision_on_top_k(cf.test_ratings['ratings'], cf.pred_test, data.test_count, top_k)}")
+